d20ec2209389021098cc8ef0a6af0e5f1f8120d9
mariely <anggemarigi@gmail.com>
2024-03-20 23:08:55 -0400

    modificacion para services

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,23 +1,8 @@
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import ServiceCard from '../components/ServiceCard';
 import { motion } from 'framer-motion';
-const servicesData = [
-  {
-    name: "Tratamientos de Spa",
-    description: "Consiente tu cuerpo con nuestros lujosos tratamientos de spa.",
-    image: "https://st5.depositphotos.com/16122460/64768/i/380/depositphotos_647687128-stock-photo-flat-lay-composition-spa-products.jpg" // Reemplaza con la URL de la imagen correspondiente
-  },
-  {
-    name: "Piscina Olímpica",
-    description: "Disfruta y relájate en nuestra piscina olímpica climatizada.",
-    image: "https://st2.depositphotos.com/4413685/6537/i/380/depositphotos_65374199-stock-photo-yellow-pool-floats-in-a.jpg" // Reemplaza con la URL de la imagen correspondiente
-  },
-  {
-    name: "Restaurante Gourmet",
-    description: "Saborea la alta cocina en nuestro exclusivo restaurante gourmet.",
-    image: "https://st.depositphotos.com/1499736/2459/i/380/depositphotos_24590415-stock-photo-chef-offering-pasta-salad-to.jpg" // Reemplaza con la URL de la imagen correspondiente
-  },
-];
+import axios from 'axios';
 
 const containerVariants = {
   hidden: { opacity: 0 },
@@ -39,52 +24,51 @@ const itemVariants = {
     }
   }
 };
+
 const Services = () => {
-  const [selectedImage, setSelectedImage] = useState('');
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/services')
+      .then(response => {
+        setServices(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener los servicios:', error);
+      });
+  }, []);
 
   return (
     <motion.div
       className="container mx-auto px-4 py-8"
-      style={{ 
-        backgroundImage: `url(${selectedImage})`, 
-        backgroundSize: 'cover', 
-        backgroundRepeat: 'no-repeat',
-        backdropFilter: 'blur(5px)',
-        opacity: 0.1,
-      }}
       variants={containerVariants}
       initial="hidden"
       animate="visible"
     >
-      {/* Añade un contenedor alrededor del título con un fondo semi-transparente */}
       <div className="text-center mb-6">
         <h2 className="text-4xl font-bold text-sky-600 inline-block p-2"
-            style={{
-              backgroundColor: 'rgba(255, 255, 255, 0.5)', // Fondo blanco semi-transparente
-              borderRadius: '10px',
-              boxShadow: '0 0 10px #fff' // Sombra blanca alrededor del texto
-            }}>
+          style={{
+            backgroundColor: 'rgba(255, 255, 255, 0.5)',
+            borderRadius: '10px',
+            boxShadow: '0 0 10px #fff'
+          }}>
           Nuestros Servicios
         </h2>
       </div>
-      <motion.div className="flex flex-wrap -m-4"
-        variants={containerVariants}
-      >
-        {servicesData.map((service, idx) => (
-          <motion.div key={idx} className="p-4 md:w-1/3"
-            variants={itemVariants}
-            onMouseEnter={() => setSelectedImage(service.image)}
-            onMouseLeave={() => setSelectedImage('')}
-          >
-            <ServiceCard 
-              serviceName={service.name} 
+      <motion.div className="flex flex-wrap -m-4" variants={containerVariants}>
+        {services.map((service, idx) => (
+          <motion.div key={idx} className="p-4 md:w-1/3" variants={itemVariants}>
+            <ServiceCard
+              serviceName={service.name}
               serviceDescription={service.description}
-              serviceImage={service.image}
+              serviceImage={`http://localhost:3001/${service.image}`} // URL completa de la imagen
             />
           </motion.div>
         ))}
       </motion.div>
     </motion.div>
-  );};
+  );
+};
+
 export default Services;
 
